45caef0db028aaf234fe1802956da3b2f885e5c3
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-04 22:20:02 -0500

    small changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer () {
     return (
         <div className='footer'>
-            <p>Made with ❤️ 2022</p>
+            <p>Made with ❤️ and React | 2022</p>
         </div>
     )
 }
